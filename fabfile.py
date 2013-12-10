5756990f20f325c00c846487bf4be14eca1fa730
Alan Orth <alan.orth@gmail.com>
2013-12-10 23:36:09 +0300

    fabfile.py: Add support for reading settings from a file

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import local
 from jinja2 import Environment, FileSystemLoader
 
+import settings
 
 template_env = Environment(loader=FileSystemLoader('.'))
 
@@ -14,8 +15,13 @@ def prepare():
 
 def build():
     prepare()
+    context = {}
+    for attr in dir(settings):
+        if not attr.startswith('__'):
+            context[attr] = getattr(settings, attr)
+
     template = template_env.get_template('source/index.html')
-    rendered_template = template.render()
+    rendered_template = template.render(**context)
     with open('presentation/index.html', 'wb') as fh:
         fh.write(rendered_template)
 
